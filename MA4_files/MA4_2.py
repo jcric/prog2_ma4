8986878040217a7a34b4ab02e5bd7512d70a2512
jcric <jonas.c.richter@me.com>
2021-10-24 18:11:48 +0200

    Added timer

--- MA4_files/MA4_2.py
+++ MA4_files/MA4_2.py
@@ -2,12 +2,16 @@
 
 from integer import Integer
 import sys 
+from time import perf_counter as pc
 
 n = int(sys.argv[1])
 
 def main():
 	f = Integer(n)
+	start = pc()
 	print(f.fib())
+	end = pc()
+	print(f"Calculting number {n} in the fibonacci series took {round(end-start, 4)} seconds.")
 
 if __name__ == '__main__':
 	main()

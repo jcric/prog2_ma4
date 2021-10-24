10572bc0b92bfa701ca098a413c376717696acd3
jcric <jonas.c.richter@me.com>
2021-10-24 18:16:39 +0200

    Improved readability

--- MA4_files/MA4_2.py
+++ MA4_files/MA4_2.py
@@ -9,9 +9,9 @@ n = int(sys.argv[1])
 def main():
 	f = Integer(n)
 	start = pc()
-	print(f.fib())
+	print(f"Number {n} in the fibonacci is {f.fib()}.")
 	end = pc()
-	print(f"Calculting number {n} in the fibonacci series took {round(end-start, 4)} seconds.")
+	print(f"Calculting this took {round(end-start, 4)} seconds.")
 
 if __name__ == '__main__':
 	main()
